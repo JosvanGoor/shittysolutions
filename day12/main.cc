b98a1bebe2b2e79f52c4c97bc3950cac3c1dd298
Jos van Goor <josvangoor@gmail.com>
2018-12-14 14:04:28 +0100

    day12 part one

--- day12/main.cc
+++ day12/main.cc
@@ -1,24 +1,30 @@
+#include <algorithm>
+#include <iomanip>
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
-struct Rule
+unordered_map<string, char> read_rules()
 {
-    string rule;
-    char result;
-};
+    unordered_map<string, char> rules;
 
-Rule read_rule()
-{
-    Rule rule;
+    while(!cin.eof())
+    {
+        char result;
+        string rule;
 
-    cin >> rule.rule;
-    cin.ignore(4); //ignore ' => '
-    cin >> rule.result;
-    cin.ignore(1); //ignore '\n'
-    return rule;
+        cin >> rule;
+        cin.ignore(4); //ignore ' => '
+        cin >> result;
+        cin.ignore(1); //ignore '\n'
+
+        rules[rule] = result;
+    }
+
+    return rules;
 }
 
 vector<char> initial_state()
@@ -30,7 +36,7 @@ vector<char> initial_state()
     return vector<char>(buf.begin(), buf.end());
 }
 
-string get_signature(vector<char> const &plants, size_t idx)
+string environment(vector<char> const &plants, size_t idx)
 {
     string sig;
     
@@ -48,26 +54,74 @@ string get_signature(vector<char> const &plants, size_t idx)
             return sig;
 
         default:
-            return string(plants.begin() + (idx - 2), plants.begin() + (idx + 2));
+        break;
     }
+
+    if (idx == (plants.size() - 2))
+    {
+        auto it = plants.begin() + idx;
+        return string(it - 2, it + 2) + '.';
+    }
+    else if (idx == (plants.size() - 1))
+    {
+        auto it = plants.begin() + idx;
+        return string(it - 2, it + 1) + "..";
+    }
+
+    auto it = plants.begin() + idx;
+    return string(it - 2, it + 3);
 }
 
 int main()
 {
     int first = 0;
     vector<char> plants = initial_state();
-    vector<Rule> rules;
+    unordered_map<string, char> rules = read_rules();
 
-    cout << string(plants.begin(), plants.end()) << "\n";
+    string original = string(plants.begin(), plants.end());
+    cout << original << "\n";
 
-    while(!cin.eof())
+    for (size_t iterations = 20; iterations--; )
     {
-        rules.push_back(read_rule());
+        if (find(plants.begin(), plants.begin() + 3, '#')
+             != plants.begin() + 3)
+        {
+            first -= 2;
+            plants.insert(plants.begin(), {'.', '.'});
+        }
 
-        cout << "Rule: ";
-        cout << rules.back().rule;
-        cout << " => " << rules.back().result << "\n";
+        if (find(plants.end() - 3, plants.end(), '#')
+            != plants.end())
+        {
+            plants.insert(plants.end(), {'.', '.'});
+        }
+        vector<char> nextgen(plants.size(), '.');
+
+        // cout << "\n\n  # now   env    new\n";
+        for (size_t idx = 0; idx != plants.size(); ++idx)
+        {
+            // cout << setw(3) << first + static_cast<int>(idx) << " ";
+            // cout << "[" << plants[idx] << "]: ";
+            string env = environment(plants, idx);
+            // cout << env << " => ";
+
+            char result = '?';
+            if (auto it = rules.find(env); it != rules.end())
+                result = it->second;
+            // cout << result << "\n";
+            nextgen[idx] = result;
+        }
+        plants.swap(nextgen);
     }
 
+    // cout << string(-first, ' ') << original << "\n";
+    // cout << string(plants.begin(), plants.end()) << "\n";
 
+    int score = 0;
+    for (size_t idx = 0; idx < plants.size(); ++idx)
+    {
+        score += (plants[idx] == '#' ? idx + first : 0);
+    }
+    cout << "score: " << score << "\n";
+    
 }
